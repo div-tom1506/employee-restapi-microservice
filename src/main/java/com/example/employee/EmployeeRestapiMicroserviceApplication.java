791516624636c5e776926ed5931744ef5aa9c4cb
div-tom1506 <divyanshuk1506@gmail.com>
2025-02-24 22:57:06 +0530

    initial commit

---
+++ src/main/java/com/example/employee/EmployeeRestapiMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRestapiMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRestapiMicroserviceApplication.class, args);
+	}
+
+}
